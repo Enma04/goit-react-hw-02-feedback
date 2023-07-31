45f6ede18154c7aa1a90156d8e629782d10cbe79
Enma04 <118761090+Enma04@users.noreply.github.com>
2023-07-31 13:03:18 +1000

    correccion error

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,10 +3,6 @@ import css from "../FeedBack/FeedBack.module.css";
 
 export default class Statistics extends Component {
 
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         const { good, neutral, bad, total, percentage } = this.props;
 
